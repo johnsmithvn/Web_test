5cd48ae021a4f922f4465aa38cac0feaad2d0546
Tung Nguyen <tungnguyen1@beeketing.net>
2025-12-16 14:49:37 +0700

    update

--- compass-vue/src/composables/useLeanbaseCompass.js
+++ compass-vue/src/composables/useLeanbaseCompass.js
@@ -1,7 +1,7 @@
 import posthog from "posthog-js";
 
 export function usePosthog() {
-  posthog.init("lbc_E1YoazZKW3OWxFODdwOkAK7aQnutKpG0px3c0uE4aDW", {
+  posthog.init("lbc_domZKKA5YmxwkUjGLmoSPvKowxgtQzRBimT6TkFLAk5", {
     api_host: "https://ingest-compass-25.leanflag.net",
     person_profiles: "identified_only", // hoặc "always"
   });
