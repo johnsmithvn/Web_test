681e7d3af12c014b7c5705ae557618700110b63d
Tung Nguyen <tungnguyen1@beeketing.net>
2025-12-24 17:56:13 +0700

    update config

--- compass-vue/src/composables/useLeanbaseCompass.js
+++ compass-vue/src/composables/useLeanbaseCompass.js
@@ -1,28 +1,33 @@
 
 
-// import posthog from "posthog-js";
-
-// export function usePosthog() {
-//   posthog.init("lbc_k91rsdLJUWyqxCsXK9gecFkK2VPWBc4ZrbBhlDilbB0", {
-//     api_host: "https://ingest-compass-25.leanflag.net",
-//     person_profiles: "identified_only", // hoặc "always"
-//   });
-
-//   return { posthog };
-// }
 import { Leanbase } from '@leanbase.com/js'
+import PostHog from 'posthog-js-lite'
+import posthog from "posthog-js";
+const key = 'lbc_k91rsdLJUWyqxCsXK9gecFkK2VPWBc4ZrbBhlDilbB0';
+
+
+
 export function usePosthog() {
-const leanbase = new Leanbase('lbc_k91rsdLJUWyqxCsXK9gecFkK2VPWBc4ZrbBhlDilbB0', {
+const leanbase = new Leanbase(key, {
   host: 'https://ingest-compass-25.leanflag.net',  // Use 'host', not 'api_host'
   person_profiles: 'identified_only'  // or 'always'
 });
   return { leanbase };
 }
-// import PostHog from 'posthog-js-lite'
+
 // export function usePosthog() {
-// const posthog = new PostHog('lbc_k91rsdLJUWyqxCsXK9gecFkK2VPWBc4ZrbBhlDilbB0', {
+// const posthog = new PostHog(key, {
 //   /* options, e.g. for self-hosted users */
 //   host: "https://ingest-compass-25.leanflag.net"
 // });
 // return { posthog }
+// }
+
+// export function usePosthog() {
+//   posthog.init(key, {
+//     api_host: "https://ingest-compass-25.leanflag.net",
+//     person_profiles: "identified_only", // hoặc "always"
+//   });
+
+//   return { posthog };
 // }
